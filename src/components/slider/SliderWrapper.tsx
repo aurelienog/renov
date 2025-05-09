a6cf9c9fc2e2676d2b9754b5e8196e9d08f46f56
Aurelie Nogueira <aurelienog@gmail.com>
2025-05-09 11:37:14 +0200

    projects section SSR

---
+++ src/components/slider/SliderWrapper.tsx
@@ -0,0 +1,28 @@
+'use client'
+import React, { useRef } from 'react';
+import { useScroll, useTransform } from 'framer-motion';
+import ParallaxCard from '../parallax-card/ParallaxCard';
+import { StaticImageData } from 'next/image';
+
+function SliderWrapper({lastProjects}: {lastProjects: StaticImageData[]}) {
+  const scrollRef= useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: scrollRef,
+    offset: ['start 20%', '15% 15%']
+  })
+
+  const maskScale = useTransform(scrollYProgress, [0, 1], ['30%', '100%'])
+  return (
+    <div ref={scrollRef}>
+        { 
+        lastProjects.map((project: StaticImageData, index:number) => {
+          const isFirst = index === 0; 
+          return <ParallaxCard project={ project } key={ index } maskSize={isFirst? maskScale : undefined}/>
+        })
+      }
+    </div>
+  )
+}
+
+export default SliderWrapper
--- src/sections-home/projects-section/ProjectsSection.tsx
+++ src/sections-home/projects-section/ProjectsSection.tsx
@@ -1,32 +1,15 @@
-'use client'
-import React, { JSX, useRef } from 'react';
-import { useScroll, useTransform } from 'framer-motion';
+import React, { JSX } from 'react';
 import styles from './styles.module.css'
 import { lastProjects } from '@/data/data';
-import { StaticImageData } from 'next/image';
-import ParallaxCard from '@/components/parallax-card/ParallaxCard';
+
+import SliderWrapper from '@/components/slider/SliderWrapper';
 
 function ProjectsSection(): JSX.Element {
-  const scrollRef= useRef(null);
-
-  const { scrollYProgress } = useScroll({
-    target: scrollRef,
-    offset: ['start 20%', '15% 15%']
-  })
-
-  const maskScale = useTransform(scrollYProgress, [0, 1], ['30%', '100%'])
 
   return (
     <section className={`section full-width ${styles.section}`}>
       <h2>Travaux récents</h2>
-      <div ref={scrollRef} className={styles.cardContainer}>
-        { 
-        lastProjects.map((project: StaticImageData, index:number) => {
-          const isFirst = index === 0; 
-          return <ParallaxCard project={ project } key={ index } maskSize={isFirst? maskScale : undefined}/>
-        })
-      }
-      </div>
+      <SliderWrapper lastProjects={lastProjects}/>
     </section>
   )
 }
