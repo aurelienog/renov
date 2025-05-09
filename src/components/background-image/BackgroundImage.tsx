3f12032c2c060c7ec0ea98bf9aefa818c15182e2
Aurelie Nogueira <aurelienog@gmail.com>
2025-05-09 14:57:13 +0200

    optimiza background image for hero section

--- src/components/background-image/BackgroundImage.tsx
+++ src/components/background-image/BackgroundImage.tsx
@@ -5,7 +5,7 @@ import styles from './styles.module.css';
 function BackgroundImage({ image }: {image: StaticImageData}): JSX.Element  {
   return (
     <figure className={styles.wrapper}>
-      <Image src={image} placeholder='blur' priority sizes='100dvw' quality={50} fill style={{ objectFit: 'cover' }} 
+      <Image src={image} placeholder='blur' priority sizes='(max-width: 768px) 100vw, (max-width: 1200px) 100vw, 100vw' quality={50} fill style={{ objectFit: 'cover' }} 
       alt="montre une cuisine spacieuse et lumineuse. L'esthétique générale est épurée, élégante et moderne. Il y a un îlot de cuisine avec quatre chaises en bois. La cuisine est éclairée par des suspensions et dispose d'une grande fenêtre avec des stores blancs." />
       <div className={styles.overlay}>
       </div>
