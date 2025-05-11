5360c4a1a8d51e4ab8a5c5297bdc6fb8e6832f79
Aurelie Nogueira <aurelienog@gmail.com>
2025-05-11 14:10:51 +0200

    add landing mobile about page

--- src/sections-about/landing-section/Landing.tsx
+++ src/sections-about/landing-section/Landing.tsx
@@ -5,9 +5,16 @@ import styles from './styles.module.css'
 
 
 function Landing(): JSX.Element {
+
   return (
     <section className={`full-width ${styles.section}`}>
       <Family image={teamImage}/>
+      <div className={styles.titles}>
+        <h1>Une histoire de famille</h1>
+        <h2 className={styles.a}>A</h2>
+        <h2 className={styles.n}>N</h2>
+        <h2 className={styles.j}>J</h2>
+      </div>
     </section>
   )
 }
