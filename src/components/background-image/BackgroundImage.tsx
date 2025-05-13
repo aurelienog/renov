473ce0341f25f40989a9d829fe36bbc77f95f098
Aurelie Nogueira <aurelienog@gmail.com>
2025-05-13 21:46:14 +0200

    add container background image componente

--- src/components/background-image/BackgroundImage.tsx
+++ src/components/background-image/BackgroundImage.tsx
@@ -5,9 +5,11 @@ import styles from './styles.module.css';
 function BackgroundImage({ image }: {image: StaticImageData}): JSX.Element  {
   return (
     <figure className={styles.wrapper}>
-      <Image src={image} placeholder='blur' priority sizes='100dvw' quality={50} fill style={{ objectFit: 'cover' }} 
-      alt="montre une cuisine spacieuse et lumineuse. L'esthétique générale est épurée, élégante et moderne. Il y a un îlot de cuisine avec quatre chaises en bois. La cuisine est éclairée par des suspensions et dispose d'une grande fenêtre avec des stores blancs." />
-      <div className={styles.overlay}>
+      <div style={{position: 'relative', width: '100%', height: '100vh'}}>
+        <Image src={image} placeholder='blur' priority sizes='100dvw' quality={50} fill style={{ objectFit: 'cover' }} 
+        alt="montre une cuisine spacieuse et lumineuse. L'esthétique générale est épurée, élégante et moderne. Il y a un îlot de cuisine avec quatre chaises en bois. La cuisine est éclairée par des suspensions et dispose d'une grande fenêtre avec des stores blancs." />
+      </div>
+    <div className={styles.overlay}>
       </div>
     </figure>
   )
