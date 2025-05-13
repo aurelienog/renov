dba86536a0af90093e2d663669414ef84ae1cf22
Aurelie Nogueira <aurelienog@gmail.com>
2025-05-13 12:02:21 +0200

    add css corner story section

--- src/sections-about/conflict-section/ConflictSection.tsx
+++ src/sections-about/conflict-section/ConflictSection.tsx
@@ -5,7 +5,7 @@ import portrait from '../../../public/assets/images/julien.webp';
 
 function ConflictSection():JSX.Element {
   return (
-    <section className={` section ${styles.section}`}>
+    <section className={`breakout ${styles.section}`}>
       <figure>
         <Image src={portrait} alt='Julien' fill placeholder='blur' style={{ objectFit: 'cover'}}/>
       </figure>
