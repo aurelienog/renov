329d5c21f556e097fc4a45b5eccad8b2d1fe1353
Aurelie Nogueira <aurelienog@gmail.com>
2025-05-15 18:17:57 +0200

    projects section CSR

--- src/lib/utils.ts
+++
@@ -1,7 +0,0 @@
-export function formatDate(date: string) {
-  return new Date(date).toLocaleDateString('fr-FR', {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric'
-  })
-}
--- src/sections-home/projects-section/ProjectsSection.tsx
+++ src/sections-home/projects-section/ProjectsSection.tsx
@@ -1,7 +1,12 @@
+'use client'
 import React, { JSX } from 'react';
 import styles from './styles.module.css'
 import { lastProjects } from '@/data/data';
-import SliderWrapper from '@/components/slider/SliderWrapper';
+import dynamic from 'next/dynamic';
+
+const SliderWrapper = dynamic(() => import('../../components/slider/SliderWrapper'), {
+  ssr: false,  // Esto desactiva el SSR para este componente
+});
 
 function ProjectsSection(): JSX.Element {
 
