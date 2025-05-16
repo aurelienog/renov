dfa05ac989c7c6594d2c08bf86bdd28655902496
Aurelie Nogueira <aurelienog@gmail.com>
2025-05-16 13:04:49 +0200

    move backgroundImage to hero section

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -7,15 +7,12 @@ import ProjectsSection from "@/sections-home/projects-section/ProjectsSection";
 import { JSX } from 'react';
 import BeforeAfterSection from "@/sections-home/before-after-section/BeforeAfterSection";
 import HowItWorksSection from "@/sections-home/how-it-works-section/HowItWorksSection";
-import BackgroundImage from "@/components/background-image/BackgroundImage";
-import heroImage from '../../../public/assets/images/hero.webp';
 
 export const dynamic = "force-static";
 
 export default function Home(): JSX.Element {
   return (
-  <main className="base-layout" style={{position: 'relative'}}>
-    <BackgroundImage image={heroImage}/>
+  <main className="base-layout">
     <HeroSection/>
     <TrustSection/>
     <ServicesSection/>
--- src/sections-home/hero-section/HeroSection.tsx
+++ src/sections-home/hero-section/HeroSection.tsx
@@ -1,16 +1,19 @@
 import React, { JSX } from 'react';
 import styles from './styles.module.css';
 import Button from '@/components/button/Button';
+import BackgroundImage from "@/components/background-image/BackgroundImage";
+import heroImage from '../../../public/assets/images/hero.webp';
 
 function HeroSection(): JSX.Element  {
   return (
     <section className={`full-width ${styles.hero}`}>
-      <div className={`${styles.info}`}>
+      <BackgroundImage image={heroImage}/>
+      <article className={`${styles.info}`}>
         <h1 className='visually-hidden'>Peintre en bâtiment, expert en peinture et rénovation intérieure/extérieure dans les Yvelines et le Val-d&#39;Oise</h1>
         <h2>Redonnez vie à vos espaces</h2>
         <p>Peintre en bâtiment, j&#39;embellis et restaure vos intérieurs dans les Yvelines et le Val d&#39;Oise.</p>
         <Button>Obtenir un devis gratuit</Button>
-      </div>      
+      </article>      
     </section>
   )
 };
