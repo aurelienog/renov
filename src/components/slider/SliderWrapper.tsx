51705d203358f3d19df3e8e76ad646213c9d5cd2
Aurelie Nogueira <aurelienog@gmail.com>
2025-06-02 18:46:44 +0200

    add CTA BTN realisation section home page

--- src/components/slider/SliderWrapper.tsx
+++ src/components/slider/SliderWrapper.tsx
@@ -1,28 +1,32 @@
 'use client'
 import React, { useRef } from 'react';
-import { useScroll, useTransform } from 'framer-motion';
+import { useInView, useScroll, useTransform } from 'framer-motion';
 import ParallaxCard from '../parallax-card/ParallaxCard';
 import { StaticImageData } from 'next/image';
-
-
+import Link from 'next/link';
+import Button from '../button/Button';
 
 function SliderWrapper({lastProjects}: {lastProjects: StaticImageData[]}) {
   const scrollRef= useRef(null);
-
   const { scrollYProgress } = useScroll({
     target: scrollRef,
     offset: ['start 20%', '15% 15%']
   })
-
   const maskScale = useTransform(scrollYProgress, [0, 1], ['30%', '100%'])
+
+  // show CTA Button
+  const isInView = useInView(scrollRef, { amount: 0.16, once: false})
+  
   return (
     <div ref={scrollRef}>
-        { 
-        lastProjects.map((project: StaticImageData, index:number) => {
+      { lastProjects.map((project: StaticImageData, index:number) => {
           const isFirst = index === 0; 
           return <ParallaxCard project={ project } key={ index } maskSize={isFirst? maskScale : undefined}/>
         })
       }
+      
+      {isInView && <Link href="/realisations"><Button>Mes réalisations</Button></Link>}
+      
     </div>
   )
 }
