33ed2c3cec5915eb65a00445eecfd6db4185840e
Aurelie Nogueira <aurelienog@gmail.com>
2025-06-29 22:00:08 +0200

    add tab navegation realisations page

--- src/components/projects-gallery/GalleryItem.tsx
+++ src/components/projects-gallery/GalleryItem.tsx
@@ -4,10 +4,17 @@ import Image from 'next/image'
 import React from 'react'
 
 function GalleryItem({project, handleClick}: { project: Project, handleClick: (p: Project) => void }) {
+
+    const handleKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === 'Enter' || e.key === ' ') {
+      e.preventDefault()
+      handleClick(project)
+    }
+  }
   
   return (
-    <li onClick={() => (handleClick(project))}>
-      <Image src={project.image} alt={project.description} sizes='(max-width: 970px) 100vw, (max-width: 1400px) 50vw, 33vw"'  placeholder='blur' style={{ width: '100%', height: 'auto', objectFit: 'cover', marginBottom: '1rem'}}/>
+    <li>
+      <Image src={project.image} alt={project.description} onClick={() => (handleClick(project))} onKeyDown={handleKeyDown} tabIndex={0} role="button" aria-label={`Ouvrir la photo dans le carrousel`} sizes='(max-width: 970px) 100vw, (max-width: 1400px) 50vw, 33vw"'  placeholder='blur' style={{ width: '100%', height: 'auto', objectFit: 'cover', marginBottom: '1rem'}}/>
     </li>
   )
 }
