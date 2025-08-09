7614e2f2bc2c3ba12d4751ff6c7675576de51e1d
Aurelie Nogueira <aurelienog@gmail.com>
2025-08-09 15:00:42 +0200

    feat: add WhySection to about page

--- src/app/a-propos/page.tsx
+++ src/app/a-propos/page.tsx
@@ -6,8 +6,8 @@ import Story from "@/sections/sections-about/story-section/StorySection";
 import styles from "./page.module.css"
 import { JSX } from 'react';
 import QuoteSection from "@/sections/sections-about/quote-section/QuoteSection";
-import Link from "next/link";
 import { Metadata } from "next";
+import WhySection from "@/sections/sections-about/why-section/WhySection";
 
 export const metadata: Metadata = {
   title: "À Propos"
@@ -22,11 +22,7 @@ export default function About(): JSX.Element {
       <ConflictSection/>
       <MissionSection/>
       <BenefitsSection/>
-      <section className="section">
-        <h2>Mon engagement</h2>
-        <p>ANJ RENOV, c{`'`}est le choix d{`'`}<span className='highlight'>un artisan investi</span>, proche de ses clients, et fier de chaque réalisation.</p>
-        <Link href={'/#contact'} className="highlight">Vous avez un projet ? Parlons-en.</Link>
-      </section>
+      <WhySection />
     </main>
   );
 }
---
+++ src/sections/sections-about/why-section/WhySection.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+import React from 'react';
+import styles from './styles.module.css';
+
+function WhySection() {
+  return (
+    <section className={`section ${styles.section}`}>
+        <h2>Mon engagement</h2>
+        <p>ANJ RENOV, c{`'`}est le choix d{`'`}<span className='highlight'>un artisan investi</span>, proche de ses clients, et fier de chaque réalisation.</p>
+        <Link href={'/#contact'} className="highlight">Vous avez un projet ? Parlons-en.</Link>
+    </section>
+  )
+}
+
+export default WhySection
