a35c62448be79c614c3b66d4e338a4712c9e7540
Aurelie Nogueira <aurelienog@gmail.com>
2025-08-24 12:00:35 +0200

    change h1 homepage

--- src/sections/sections-home/hero-section/HeroSection.tsx
+++ src/sections/sections-home/hero-section/HeroSection.tsx
@@ -10,8 +10,7 @@ function HeroSection(): JSX.Element  {
       <div aria-hidden="true" className={styles.clip}></div>
       <BackgroundImage image={heroImage} overlay={true}/>
       <article className={`${styles.info}`}>
-        <h1 className='visually-hidden' aria-hidden="true">Peintre en bâtiment, expert en peinture et rénovation intérieure/extérieure dans les Yvelines et le Val-d{`'`}Oise</h1>
-        <h2 id="accueil">Redonnez vie à vos espaces</h2>
+        <h1 id="accueil">Redonnez vie à vos espaces</h1>
         <p>Peintre en bâtiment, j{`'`}embellis et restaure vos intérieurs dans les Yvelines et le Val d{`'`}Oise.</p>
         <Link href={"/#contact"} className='button'>Obtenir un devis gratuit</Link>
       </article>      
